ae22df41a872ed0baa9b3ba0405719018bf44b8e
Sanchit Vijay <71404343+sanchiitvijay@users.noreply.github.com>
2024-07-10 02:40:01 +0530

    Update 7b.py

--- 7b.py
+++ 7b.py
@@ -1,31 +1,22 @@
 import numpy as np
 
-def display_2d_array(arr):
-    print("Original 2D Array:")
-    print(arr)
-
-def display_reverse_array(arr):
-    print("\nArray elements in reverse order:")
-    print(arr[::-1, ::-1])
-
-def display_principal_diagonal(arr):
-    print("\nPrincipal diagonal elements:")
-    print(np.diag(arr))
-
-def sort_array(arr):
-    sorted_asc = np.sort(arr, axis=None)
-    print("\nSorted 2D Array in ascending order:")
-    print(sorted_asc.reshape(arr.shape))
-
-    sorted_desc = np.sort(arr, axis=None)[::-1]
-    print("\nSorted 2D Array in descending order:")
-    print(sorted_desc.reshape(arr.shape))
-
 arr = np.array([[1, 2, 3],
                 [4, 5, 6],
                 [7, 8, 9]])
 
-display_2d_array(arr)
-display_reverse_array(arr)
-display_principal_diagonal(arr)
-sort_array(arr)
+print("Original 2D Array:")
+print(arr)
+
+print("\nArray elements in reverse order:")
+print(arr[::-1, ::-1])
+
+print("\nPrincipal diagonal elements:")
+print(np.diag(arr))
+
+sorted_asc = np.sort(arr, axis=None)
+print("\nSorted 2D Array in ascending order:")
+print(sorted_asc.reshape(arr.shape))
+
+sorted_desc = np.sort(arr, axis=None)[::-1]
+print("\nSorted 2D Array in descending order:")
+print(sorted_desc.reshape(arr.shape))
